9fe7dd4309f61def9de65624f3b3192d1bb72ae7
giamyiweijeremystudy <giamyiweijeremy.study@gmail.com>
2026-01-30 10:05:58 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,33 @@
+let db;
 
+export function initDB() {
+return new Promise(resolve => {
+const req = indexedDB.open('taskcal-db', 1);
+
+
+req.onupgradeneeded = e => {
+db = e.target.result;
+db.createObjectStore('items', { keyPath: 'id' });
+};
+
+
+req.onsuccess = e => {
+db = e.target.result;
+resolve();
+};
+});
+}
+
+export function saveItem(item) {
+const tx = db.transaction('items', 'readwrite');
+tx.objectStore('items').put(item);
+}
+
+
+export function getItems() {
+return new Promise(resolve => {
+const tx = db.transaction('items');
+const req = tx.objectStore('items').getAll();
+req.onsuccess = () => resolve(req.result);
+});
+}
